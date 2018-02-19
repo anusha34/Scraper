1f15030fab94c3672c0a14228bd17293ba6dea4c
dev-admin <anusha@korus.co>
2018-02-19 15:31:30 -0500

    Changes in functions

--- Uni_scrape.py
+++ Uni_scrape.py
@@ -16,28 +16,28 @@ dictList = []
 
 university_urls = {
     'Immaculata' : 'http://www.immaculata.edu/titleix',
-    'Ursinus' : 'http://www.ursinus.edu/student-life/handbook/sexual-and-gender-based-misconduct/'
+    'Ursinus' : 'http://www.ursinus.edu/student-life/handbook/sexual-and-gender-based-misconduct'
     }
 
 def parse_ursinus(k = "Ursinus"):
     # class for ursinus : lw_subnav (for links) and editable (for content)
     urls = university_urls[k]
     page = urlopen(urls)
-    # print(url)
     soup = BeautifulSoup(page, 'html.parser')
     c_list = soup.find(class_='lw_subnav')
     c_list_items = c_list.find_all('a')
     for c_name in c_list_items:
-        names = c_name.get_text()
         link = c_name.get('href')
-        html_page = urlopen(link)
+        links = urls + link
+        html_page = urlopen(links)
         soup = BeautifulSoup(html_page, 'html.parser')
         con_list = soup.find(class_='editable')
-        con_items = con_list.find_all('div')
-        for con_name in con_items:
-            names = con_name.get_text().encode('UTF8')
-            s = open(k + '.txt', 'a+')
-            s.write(str(names) + "\n")
+        print(con_list)
+  #      con_items = con_list.find_all('div')
+   #     for con_name in con_items:
+    #        names = con_name.get_text().encode('UTF8')
+     #       s = open(k + '.txt', 'a+')
+      #      s.write(str(names) + "\n")
     print('Parsed Ursinus')
 
 #region region-content class for immaculata
