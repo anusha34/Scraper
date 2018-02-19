26021d88ddb309a61090cdad2518f2c38613936f
dev-admin <anusha@korus.co>
2018-02-19 16:20:57 -0500

    Updated functions

--- Uni_scrape.py
+++ Uni_scrape.py
@@ -2,13 +2,7 @@
 from bs4 import BeautifulSoup
 import json
 from urllib.request import urlopen
-import re
 import requests
-
-#from urllib3 import request
-#import nltk
-#import webbrowser
-#import sys
 
 dictList = []
 
@@ -19,26 +13,25 @@ university_urls = {
     'Ursinus' : 'http://www.ursinus.edu/student-life/handbook/sexual-and-gender-based-misconduct'
     }
 
-def parse_ursinus(k = "Ursinus"):
-    # class for ursinus : lw_subnav (for links) and editable (for content)
+
+# class for ursinus : lw_subnav (for links) and editable (for content)
+def parse_ursinus(k = 'Ursinus'):
     urls = university_urls[k]
     page = urlopen(urls)
     soup = BeautifulSoup(page, 'html.parser')
     c_list = soup.find(class_='lw_subnav')
     c_list_items = c_list.find_all('a')
     for c_name in c_list_items:
-        link = c_name.get('href')
-        links = urls + link
-        print(links)
-     #   html_page = urlopen(links)
-    #    soup = BeautifulSoup(html_page, 'html.parser')
-   #     con_list = soup.find(class_='editable')
-  #      print(con_list)
-  #      con_items = con_list.find_all('div')
-   #     for con_name in con_items:
-    #        names = con_name.get_text().encode('UTF8')
-     #       s = open(k + '.txt', 'a+')
-      #      s.write(str(names) + "\n")
+        url = c_name.get('href')
+        link = 'http://www.ursinus.edu' + url
+        html_page = urlopen(link)
+        soup = BeautifulSoup(html_page, 'html.parser')
+        content_list = soup.find(id='main')
+        content_items = content_list.find_all(['p', 'ul'])
+        for content_items_list in content_items:
+            names = content_items_list.get_text().encode('UTF8')
+            s = open(k + '.txt', 'a+')
+            s.write(str(names) + "\n")
     print('Parsed Ursinus')
 
 #region region-content class for immaculata
