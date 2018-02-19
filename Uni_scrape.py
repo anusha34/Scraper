a2dfcc668d167eb7280987b6ac06add339d51c7e
dev-admin <anusha@korus.co>
2018-02-19 16:33:19 -0500

    Updated functions

--- Uni_scrape.py
+++ Uni_scrape.py
@@ -13,10 +13,29 @@ university_urls = {
     'Ursinus' : 'http://www.ursinus.edu/student-life/handbook/sexual-and-gender-based-misconduct'
     }
 
+#region region-content class for immaculata
+def parse_immaculata(key):
+    urls = university_urls[key]
+    page = urlopen(urls)
+    soup = BeautifulSoup(page, 'html.parser')
+    c_list = soup.find(class_='region region-content')
+    c_list_items = c_list.find_all('a')
+    for c_name in c_list_items:
+        names = c_name.get_text()
+        link = c_name.get('href')
+        html_page = urlopen(link)
+        soup = BeautifulSoup(html_page, 'html.parser')
+        con_list = soup.find(class_='region region-content')
+        con_items = con_list.find_all('div')
+        for con_name in con_items:
+            names = con_name.get_text().encode('UTF8')
+            s = open(key + '.txt', 'a+')
+            s.write(str(names) + "\n")
+    print('Parsed Immaculata')
 
 # class for ursinus : lw_subnav (for links) and editable (for content)
-def parse_ursinus(k = 'Ursinus'):
-    urls = university_urls[k]
+def parse_ursinus(key):
+    urls = university_urls[key]
     page = urlopen(urls)
     soup = BeautifulSoup(page, 'html.parser')
     c_list = soup.find(class_='lw_subnav')
@@ -30,30 +49,9 @@ def parse_ursinus(k = 'Ursinus'):
         content_items = content_list.find_all(['p', 'ul'])
         for content_items_list in content_items:
             names = content_items_list.get_text().encode('UTF8')
-            s = open(k + '.txt', 'a+')
+            s = open(key + '.txt', 'a+')
             s.write(str(names) + "\n")
     print('Parsed Ursinus')
 
-#region region-content class for immaculata
-def parse_immaculata(k = "Immaculata"):
-    urls = university_urls[k]
-    page = urlopen(urls)
-    #print(url)
-    soup = BeautifulSoup(page, 'html.parser')
-    c_list = soup.find(class_='region region-content')
-    c_list_items = c_list.find_all('a')
-    for c_name in c_list_items:
-        names = c_name.get_text()
-        link = c_name.get('href')
-        html_page = urlopen(link)
-        soup = BeautifulSoup(html_page, 'html.parser')
-        con_list = soup.find(class_='region region-content')
-        con_items = con_list.find_all('div')
-        for con_name in con_items:
-            names = con_name.get_text().encode('UTF8')
-            s = open(k + '.txt', 'a+')
-            s.write(str(names) + "\n")
-    print('Parsed Immaculata')
-
-parse_ursinus()
-parse_immaculata()
+parse_immaculata("Immaculata")
+parse_ursinus("Ursinus")
