6d6145075c80a1851fd07b2b525b7b0d245bc55f
dev-admin <anusha@korus.co>
2018-02-20 12:20:17 -0500

    Updated functions

--- Uni_scrape.py
+++ Uni_scrape.py
@@ -4,21 +4,11 @@ import json
 from urllib.request import urlopen
 import requests
 
-dictList = []
-
-#universities = ['Immaculata','Ursinus']
-
-university_urls = {
-    'Immaculata' : 'http://www.immaculata.edu/titleix',
-    'Ursinus' : 'http://www.ursinus.edu/student-life/handbook/sexual-and-gender-based-misconduct'
-    }
-
 #region region-content class for immaculata
-def parse_immaculata(key):
-    urls = university_urls[key]
-    page = urlopen(urls)
+def parse_immaculata(page):
     soup = BeautifulSoup(page, 'html.parser')
     c_list = soup.find(class_='region region-content')
+    print(c_list)
     c_list_items = c_list.find_all('a')
     for c_name in c_list_items:
         names = c_name.get_text()
@@ -29,14 +19,13 @@ def parse_immaculata(key):
         con_items = con_list.find_all('div')
         for con_name in con_items:
             names = con_name.get_text().encode('UTF8')
-            s = open(key + '.txt', 'a+')
-            s.write(str(names) + "\n")
+            print(names)
+            #s = open(key + '.txt', 'a+')
+            #s.write(str(names) + "\n")
     print('Parsed Immaculata')
 
 # class for ursinus : lw_subnav (for links) and editable (for content)
-def parse_ursinus(key):
-    urls = university_urls[key]
-    page = urlopen(urls)
+def parse_ursinus(page):
     soup = BeautifulSoup(page, 'html.parser')
     c_list = soup.find(class_='lw_subnav')
     c_list_items = c_list.find_all('a')
@@ -49,9 +38,29 @@ def parse_ursinus(key):
         content_items = content_list.find_all(['p', 'ul'])
         for content_items_list in content_items:
             names = content_items_list.get_text().encode('UTF8')
-            s = open(key + '.txt', 'a+')
-            s.write(str(names) + "\n")
+            print(names)
+            #s = open(key + '.txt', 'a+')
+            #s.write(str(names) + "\n")
     print('Parsed Ursinus')
 
-parse_immaculata("Immaculata")
-parse_ursinus("Ursinus")
+
+universities = ["Immaculata" , "Ursinus"]
+
+university_urls = {
+    'Immaculata' : 'http://www.immaculata.edu/titleix',
+    'Ursinus' : 'http://www.ursinus.edu/student-life/handbook/sexual-and-gender-based-misconduct'
+    }
+
+
+university_funcs =  {
+    'Immaculata' : parse_immaculata,
+    'Ursinus' : parse_ursinus
+}
+
+result_dict = {}
+
+for key in universities:
+    urls = university_urls[key]
+    print(urls)
+    pg = urlopen(urls)
+    university_funcs[key](pg)
