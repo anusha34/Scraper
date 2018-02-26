8ab70fbd8b7ee326ca66e066203f01b63043c970
dev-admin <anusha@korus.co>
2018-02-26 12:27:10 -0500

    Prints title IX data for each university in a dictionary.

--- Uni_scrape.py
+++ Uni_scrape.py
@@ -1,11 +1,10 @@
-
 from bs4 import BeautifulSoup
 import json
 from urllib.request import urlopen
 import requests
 
 
-#region region-content class for immaculata
+# region region-content class for immaculata
 def parse_immaculata(page):
     soup = BeautifulSoup(page, 'html.parser')
     c_list = soup.find(class_='region region-content')
@@ -14,7 +13,7 @@ def parse_immaculata(page):
         content = c_name.get_text()
         link = c_name.get('href')
         ima_dict = {
-            #"context": text,
+            # "context": text,
             "url": link,
             "content": content
         }
@@ -26,10 +25,10 @@ def parse_immaculata(page):
             text_list = con_name.get_text().encode('UTF8')
         ima_dict["text"] = str(text_list)
         dict_list1.append(ima_dict)
-        title_ix(dict_list1)
-    #print(dictList1)
-            #s = open(key + '.txt', 'a+')
-            #s.write(str(names) + "\n")
+        #title_ix(dict_list1)
+    # print(dictList1)
+    # s = open(key + '.txt', 'a+')
+    # s.write(str(names) + "\n")
     print('Parsed Immaculata')
     return dict_list1
 
@@ -44,51 +43,55 @@ def parse_ursinus(page):
         url = c_name.get('href')
         link = 'http://www.ursinus.edu' + url
         urs_dict = {
-            #"text": text,
+            # "text": text,
             "url": link,
             "content": content
-            }
+        }
         html_page = urlopen(link)
         soup = BeautifulSoup(html_page, 'html.parser')
         con_list = soup.find(id='main')
-        con_items = con_list.find_all([ 'p', 'ul' ])
+        con_items = con_list.find_all(['p', 'ul'])
         for con_name in con_items:
             text_list = con_name.get_text().encode('UTF8')
         urs_dict["text"] = str(text_list)
         dict_list2.append(urs_dict)
-        title_ix(dict_list2)
-    #print(dictList2)
-        #print(dictList2)
-            #s = open(key + '.txt', 'a+')
-            #s.write(str(names) + "\n")
+        #title_ix(dict_list2)
+    # print(dictList2)
+    # print(dictList2)
+    # s = open(key + '.txt', 'a+')
+    # s.write(str(names) + "\n")
     print('Parsed Ursinus')
     return dict_list2
-
-def title_ix(dict_list1, dict_list2):
-    title_ix_data = {
-        "Immaculata": dict_list1,
-        "Ursinus": dict_list2
-    }
-    print("visited")
-    print(title_ix_data)
 
 
 dict_list1 = []
 dict_list2 = []
+
+title_ix_data = {}
+
 universities = ["Immaculata", "Ursinus"]
-university_urls = {
-    'Immaculata': 'http://www.immaculata.edu/titleix',
-    'Ursinus': 'http://www.ursinus.edu/student-life/handbook/sexual-and-gender-based-misconduct'
-    }
+
 university_funcs = {
     'Immaculata': parse_immaculata,
     'Ursinus': parse_ursinus
 }
 
+university_urls = {
+    'Immaculata': 'http://www.immaculata.edu/titleix',
+    'Ursinus': 'http://www.ursinus.edu/student-life/handbook/sexual-and-gender-based-misconduct'
+}
+
 for key in universities:
     url = university_urls[key]
-    #print(url)
+    # print(url)
     pg = urlopen(url)
-    university_funcs[key](pg)
+    x = university_funcs[key](pg)
+    title_ix_data[key] = x
+print(title_ix_data)
+
+
+#for university in universities:
+#    title_ix_data[university] = university_funcs[university]()
+#print(title_ix_data)
 
 
